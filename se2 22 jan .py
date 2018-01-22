88a6c66cde68ff41bdbd29b4cdbd289dae30a38a
nithingowdanikki <35587832+nithingowdanikki@users.noreply.github.com>
2018-01-22 16:18:47 +0530

    Add files via upload

---
+++ se2 22 jan .py
@@ -0,0 +1,38 @@
+roll
+if count==8:
+   count=37
+print("its a ladder,climb up")
+if count==11:
+   count=2
+print("its a snake!!,come down")
+if count==13:
+   count=34
+print("its a ladder,climb up")
+if count==25:
+   count=4
+print("its a snake!!,come down")
+if count==38:
+   count=9
+print("its a snake!!,come down")
+if count==40:
+   count=68
+print("its a snake!!,come down")
+if count==52:
+   count=81
+print("its a ladder,climb up")
+if count==65:
+   count=46
+print("its a snake!!,come down")
+if count==76:
+   count=97
+print("its a ladder,climb up")
+if count==89:
+   count=70
+print("its a snake!!,come down")
+if count==97
+   count=64
+print("its a snake!!,come down")
+
+
+
+
